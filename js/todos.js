b7c054a56d2206784ace60cd7083bbfaad692867
Matej <matejchalachan@gmail.com>
2020-08-22 21:34:11 +0200

    todos  filter and editing

---
+++ js/todos.js
@@ -0,0 +1,70 @@
+window.todos = function () {
+
+	return {
+		todos: [],
+
+		newTodo: '',
+
+		editedTodo: null,
+
+		filter: 'all',
+
+		get filteredTodos() {
+
+			return {
+				all: this.todos,
+				active: this.active,
+				completed: this.completed
+			}[this.filter];
+		},
+
+		get active() {
+			return this.todos.filter(todo => !todo.completed)
+		},
+
+		get completed() {
+			return this.todos.filter(todo => todo.completed);
+		},
+
+		addTodo() {
+			this.todos.push({
+				id: this.todos.length + 1,
+				body: this.newTodo,
+				completed: false,
+			});
+			this.newTodo = '';
+		},
+
+		deleteTodo(todo) {
+			let position = this.todos.indexOf(todo);
+			this.todos.splice(position,1);
+		},
+
+		completeTodo(todo) {
+			todo.completed = !todo.completed;
+		},
+
+		editTodo(todo) {
+			todo.cachedBody = todo.body;
+
+			this.editedTodo = todo;
+		},
+
+		cancelEdit(todo) {
+			todo.body = todo.cachedBody;
+
+			this.editedTodo = null;
+
+			delete todo.cachedBody;
+		},
+
+		editComplete(todo) {
+			if(todo.body.trim() === '') {
+				this.deleteTodo(todo);
+			}
+
+			this.editedTodo = null;
+		}
+	}
+
+}
